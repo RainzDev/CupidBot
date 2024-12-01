a821b12a385c07e1d5278fa7234a1c46cb5159da
ProudGuy69 <idioticbatteringram@gmail.com>
2024-12-01 16:02:02 -0500

    add and track entrymodal.py

---
+++ entrymodal.py
@@ -0,0 +1,7 @@
+from discord.ui import Select, View, Modal, TextInput, button, DynamicItem
+from discord import Button, ButtonStyle, Interaction, TextStyle, Embed, SelectOption
+
+# modal to ask ppl questions when they join the server
+
+class EntryModal(Modal):
+    pass
